a6479b42649aba4d4b8e8d97e471136f8ac8bd8d
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-02-10 17:15:45 +0200

    file transfer

---
+++ src/components/App.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/FIlter';
+import ContactList from './ContactList/ContactList';
+import css from './App.module.scss';
+
+export default class App extends Component {
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: "",
+  };
+
+  contactCreated = ({ name, number }) => {
+    const { contacts } = this.state;
+    if (contacts.find((contact) => contact.name.toLowerCase() === name.toLowerCase())) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      const contact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+      this.setState(({ contacts }) => ({contacts: [...contacts, contact],
+      }));
+    }
+  };
+  searchContact = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  deleteContact = (Id) => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter((contact) => contact.id !== Id),
+    }));
+  };
+
+  getContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((filter) =>
+      filter.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+  return (
+    <div className={css.app}>
+      <h1 className={css.appHeader}>PhoneBook</h1>
+      <ContactForm onSubmit={this.contactCreated} />
+      <div>
+        <h2 className={css.appHeaderSec}>Contacts</h2>
+        <Filter value={filter} change={this.searchContact} />
+        {contacts.length > 0 ? (<ContactList filter={this.getContacts()} removeContact={this.deleteContact} />) : (<p>Your Phonebook is empty!</p>)}
+      </div>
+    </div>
+    );
+  }
+};
